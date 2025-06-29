73ce7f0ea8f3070c622b5017ff7c50d8768687c8
minaemil10 <minaemile3@gmail.com>
2025-06-29 23:14:58 +0300

    solved warning

--- lib/screens/add_place_form.dart
+++ lib/screens/add_place_form.dart
@@ -1,23 +1,43 @@
-import 'dart:io';
-
-import 'package:favourite_places/widgets/pick_image.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:favourite_places/widgets/pick_image.dart';
 import 'package:favourite_places/providers/fav_places_provider.dart';
 
-class AddPlaceForm extends ConsumerWidget {
-  AddPlaceForm({super.key});
+class AddPlaceForm extends ConsumerStatefulWidget {
+  const AddPlaceForm({super.key});
+
+  @override
+  ConsumerState<AddPlaceForm> createState() => _AddPlaceFormState();
+}
+
+class _AddPlaceFormState extends ConsumerState<AddPlaceForm> {
   final formKey = GlobalKey<FormState>();
   File? image;
   String title = '';
+
+  void onAddPlace() {
+    if (formKey.currentState!.validate()) {
+      if (image != null) {
+        formKey.currentState!.save();
+        ref.read(favPlacesProvider.notifier).addPlace(title, image!);
+        Navigator.pop(context); // Close the bottom sheet
+      } else {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Must Pick an image!')));
+      }
+    }
+  }
+
   void onPickImage(File? img) {
     image = img;
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Add Place')),
       body: Form(
@@ -45,19 +65,7 @@ class AddPlaceForm extends ConsumerWidget {
               PickImage(onPick: onPickImage),
               const SizedBox(height: 20),
               ElevatedButton.icon(
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-                    if (image != null) {
-                      formKey.currentState!.save();
-                      ref.read(favPlacesProvider.notifier).addPlace(title, image!);
-                      Navigator.pop(context); // Close the bottom sheet
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Must Pick an image!')),
-                      );
-                    }
-                  }
-                },
+                onPressed: onAddPlace,
                 icon: Icon(Icons.add),
                 label: Text('Add Place'),
               ),
